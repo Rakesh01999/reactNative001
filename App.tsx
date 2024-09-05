1e4892a3e185ef992a8f402b35ea13d977d1da2f
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-05 14:54:22 +0600

    image component added

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 import {
   View,
   Text,
   SafeAreaView,
-  ScrollView,
-  Button
+  Image
 } from 'react-native'
-import useToggle from "./customHooks/useToggle";
 
 const App = () => {
-  const [isOn, toggleOn]= useToggle(false);
+  const [imageSource, setImageSource] = useState({uri:'http://example.com'})
   return (
     <SafeAreaView>
       <View>
@@ -18,13 +16,29 @@ const App = () => {
           <Text style={{ color: 'white', fontSize: 20 }}>React Native by Rakesh</Text>
         </View>
         <View style={{ backgroundColor: 'green', height: 50, paddingVertical: 5, paddingHorizontal: 100, alignItems: "center" }}>
-          <Text style={{ color: 'yellow', fontSize: 20 }}>Custom Hook</Text>
+          <Text style={{ color: 'yellow', fontSize: 20 }}>Image Component</Text>
         </View>
       </View>
 
-        <Text>{isOn ? 'ON' : 'OFF'}</Text>
-      <Button title={"Toggle"} onPress={toggleOn}></Button>
-      
+      <View>
+        {/* <Image source={require('./assets/images/cake.png')} */}
+        <Image source={imageSource}
+          // style={{ height: 200, width: 200 , backgroundColor:'red'}}
+          style={{ height: '100%', width: '100%' , backgroundColor:'red'}}
+          // resizeMode={"contain"}
+          // resizeMode={"stretch"}
+          // resizeMode={"repeat"}
+          resizeMode={"center"}
+          onError={()=>{
+            console.log('Error has been detected while loading an image')
+            setImageSource(require('./assets/images/cake.png'))
+          }}
+          />
+      </View>
+      <View>
+        <Image source={{ uri: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80" }}
+          style={{ height: 300, width: 300 }} />
+      </View>
     </SafeAreaView>
   )
 }
--- README.md
+++ README.md
@@ -450,6 +450,38 @@ const App = () => {
 
 ```
 
+## Image Component -code
+```
+const App = () => {
+  const [imageSource, setImageSource] = useState({uri:'http://example.com'})
+  return (
+    <SafeAreaView>
+          <View>
+        {/* <Image source={require('./assets/images/cake.png')} */}
+        <Image source={imageSource}
+          // style={{ height: 200, width: 200 , backgroundColor:'red'}}
+          style={{ height: '100%', width: '100%' , backgroundColor:'red'}}
+          // resizeMode={"contain"}
+          // resizeMode={"stretch"}
+          // resizeMode={"repeat"}
+          resizeMode={"center"}
+          onError={()=>{
+            console.log('Error has been detected while loading an image')
+            setImageSource(require('./assets/images/cake.png'))
+          }}
+          />
+      </View>
+      <View>
+        <Image source={{ uri: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80" }}
+          style={{ height: 300, width: 300 }} />
+      </View>
+    </SafeAreaView>
+  )
+}
+      
+```
+
+
 
 
 This is a new [**React Native**](https://reactnative.dev) project, bootstrapped using [`@react-native-community/cli`](https://github.com/react-native-community/cli).
