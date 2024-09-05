91858f7f0771c7bdb84b8848d954d611dfba84f1
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-05 11:07:16 +0600

    refs & useRef added

--- App.tsx
+++ App.tsx
@@ -1,44 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MyText from "./Components/MyText/MyText";
 import Item from "./Components/Item/Item";
 
 import {
   View,
   Text,
-  SafeAreaView
+  SafeAreaView,
+  ScrollView,
+  Button
 } from 'react-native'
 import style from "./Components/MyText/style";
 
 
 const App = () => {
-  const [text, setText] = useState(0);
+  let array = Array(300).fill(0);
+  const ScrollViewRef = useRef(null);
 
-  // console.log('Hi !', text)
-  useEffect(()=> {
-    console.log('The text has changed !')
-  }, [text])
-  useEffect(()=> {
-    console.log('The component has rendered !')
-  }, [])
+  const handleClick = () => { 
+    ScrollViewRef.current.scrollTo({x:0, y:0, animated:true}) ;
+  };
   return (
     <SafeAreaView>
       <View>
-      <View style={{backgroundColor: 'blue', height: 100,  paddingTop:20, paddingHorizontal:100}}>
-          <Text style={{color:'white', fontSize:20}}>React Native by Rakesh</Text>
+        <View style={{ backgroundColor: 'blue', height: 100, paddingTop: 20, paddingHorizontal: 100 }}>
+          <Text style={{ color: 'white', fontSize: 20 }}>React Native by Rakesh</Text>
+        </View>
+        <View style={{ backgroundColor: 'green', height: 50, paddingVertical: 5, paddingHorizontal: 100, alignItems: "center" }}>
+          <Text style={{ color: 'yellow', fontSize: 20 }}>refs and useRef</Text>
+        </View>
       </View>
-      <View style={{backgroundColor: 'green', height: 50, paddingVertical:5, paddingHorizontal:100, alignItems:"center"}}>
-          <Text style={{color:'yellow', fontSize:20}}>useEffect</Text>
-      </View>
-      </View>
-      <Item name={'Chair'} price={100}></Item>
-      <Item name={'Table'} price={340}></Item>
-      <Item name={'Desk'} price={210}></Item>
-      <View>
-        <MyText/>
-      </View>
-      <View style={{backgroundColor:'orange', width:100, alignItems:"center", marginVertical:20, marginHorizontal:150}}>
-        <Text style={{color:'black'}} onPress={()=> setText(text+1)}>{text}</Text>
-      </View>
+
+
+
+      <Button onPress={handleClick} title="Scroll To Top"></Button>
+
+      <ScrollView ref={ScrollViewRef} style={{marginBottom:300}}>
+        {array.map((value, index: number) => <Text key={index} style={{ color: 'black' }}>{index}  Hello World</Text>)}
+      </ScrollView>
+        
     </SafeAreaView>
   )
 }
--- README.md
+++ README.md
@@ -348,6 +348,42 @@ const App = () => {
 
 ```
 
+## refs and useRef -code
+```
+const App = () => {
+  let array = Array(300).fill(0);
+  const ScrollViewRef = useRef(null);
+
+  const handleClick = () => { 
+    ScrollViewRef.current.scrollTo({x:0, y:0, animated:true}) ;
+  };
+  return (
+    <SafeAreaView>
+      <View>
+        <View style={{ backgroundColor: 'blue', height: 100, paddingTop: 20, paddingHorizontal: 100 }}>
+          <Text style={{ color: 'white', fontSize: 20 }}>React Native by Rakesh</Text>
+        </View>
+        <View style={{ backgroundColor: 'green', height: 50, paddingVertical: 5, paddingHorizontal: 100, alignItems: "center" }}>
+          <Text style={{ color: 'yellow', fontSize: 20 }}>refs and useRef</Text>
+        </View>
+      </View>
+
+
+
+      <Button onPress={handleClick} title="Scroll To Top"></Button>
+
+      <ScrollView ref={ScrollViewRef} style={{marginBottom:300}}>
+        {array.map((value, index: number) => <Text key={index} style={{ color: 'black' }}>{index}  Hello World</Text>)}
+      </ScrollView>
+        
+    </SafeAreaView>
+  )
+}
+
+```
+
+
+
 This is a new [**React Native**](https://reactnative.dev) project, bootstrapped using [`@react-native-community/cli`](https://github.com/react-native-community/cli).
 
 # Getting Started
